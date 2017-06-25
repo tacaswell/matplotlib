d3fdb5d216c5b8ca782ce1ed508e989a62729626
Thomas A Caswell <tcaswell@gmail.com>
2017-06-25 18:18:30 -0400

    TST: add pathological diagonal test

--- lib/matplotlib/tests/test_simplification.py
+++ lib/matplotlib/tests/test_simplification.py
@@ -4,7 +4,7 @@ from __future__ import (absolute_import, division, print_function,
 import io
 
 import numpy as np
-from numpy.testing import assert_array_almost_equal
+from numpy.testing import assert_array_almost_equal, assert_array_equal
 
 import pytest
 
@@ -137,6 +137,52 @@ def test_antiparallel_simplification():
                                [ 0. ,  1. ],
                                [ 1. ,  0.5]],
                               simplified.vertices[:-2, :])
+
+@pytest.mark.parametrize('angle', [0, np.pi/4, np.pi/3,
+                                   np.pi/2, np.pi])
+@pytest.mark.parametrize('offset', [0, .5])
+def test_angled_antiparallel(angle, offset):
+    scale = 5
+    np.random.seed(19680801)
+    # get 15 random offsets
+    vert_offsets = (np.random.rand(15) - offset) * scale
+    # always start at 0 so rotation makes sense
+    vert_offsets[0] = 0
+    # always take the first step the same direction
+    vert_offsets[1] = 1
+    # compute points along a diagonal line
+    x = np.sin(angle) * vert_offsets
+    y = np.cos(angle) * vert_offsets
+
+    # will check these later
+    x_max = x[1:].max()
+    x_min = x[1:].min()
+
+    y_max = y[1:].max()
+    y_min = y[1:].min()
+
+    if offset:
+        p_expected = Path([[0, 0],
+                           [x_max, y_max],
+                           [x_min, y_min],
+                           [x[-1], y[-1]],
+                           [0, 0]],
+                          codes=[1, 2, 2, 2, 0])
+
+    else:
+        p_expected = Path([[0, 0],
+                           [x_max, y_max],
+                           [x[-1], y[-1]],
+                           [0, 0]],
+                          codes=[1, 2, 2, 0])
+
+
+    p = Path(np.vstack([x, y]).T)
+    p2 = p.cleaned(simplify=True)
+
+    assert_array_almost_equal(p_expected.vertices,
+                              p2.vertices)
+    assert_array_equal(p_expected.codes, p2.codes)
 
 def test_sine_plus_noise():
     np.random.seed(0)
