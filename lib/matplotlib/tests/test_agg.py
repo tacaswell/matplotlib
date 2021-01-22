471e52952c7776c64ce4b8a7d5e93fb97aeddf0c
Thomas A Caswell <tcaswell@gmail.com>
2021-01-22 17:48:31 -0500

    TST: make long path test more pathological
    Random data will sometimes go in the same direction.

--- lib/matplotlib/tests/test_agg.py
+++ lib/matplotlib/tests/test_agg.py
@@ -72,12 +72,12 @@ def test_marker_with_nan():
 
 def test_long_path():
     buff = io.BytesIO()
-
-    fig, ax = plt.subplots()
-    np.random.seed(0)
-    points = np.random.rand(70000)
+    fig = Figure()
+    ax = fig.subplots()
+    points = np.ones(100_000)
+    points[::2] *= -1
     ax.plot(points)
-    fig.savefig(buff, format='png')
+    fig.savefig(buff, format='png', dpi=500)
 
 
 @image_comparison(['agg_filter.png'], remove_text=True)
