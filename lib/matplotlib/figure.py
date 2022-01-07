e444d55f813924584a637de911f0cd9e7b7aa744
Jay Stanley <jay.s.stanley.3@gmail.com>
2022-01-06 20:03:07 -0600

    clear subfigures first

--- lib/matplotlib/figure.py
+++ lib/matplotlib/figure.py
@@ -949,6 +949,10 @@ default: %(va)s
         self.suppressComposite = None
         self.callbacks = cbook.CallbackRegistry()
 
+        for subfig in self.subfigs: 
+            #first clear the axes in any subfigures
+            subfig.clf(keep_observers=keep_observers)
+
         for ax in tuple(self.axes):  # Iterate over the copy.
             ax.cla()
             self.delaxes(ax)  # Remove ax from self._axstack.
