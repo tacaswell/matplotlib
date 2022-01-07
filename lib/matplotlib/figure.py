6bb1994c70d2849ef9e2204f338cf4efa6d2c04e
Jay Stanley <jay.s.stanley.3@gmail.com>
2022-01-07 08:34:26 -0600

    Clear all children subfigs in parent

--- lib/matplotlib/figure.py
+++ lib/matplotlib/figure.py
@@ -962,7 +962,7 @@ default: %(va)s
             toolbar.update()
         self._axstack.clear()
         self.artists = []
-        self.subfigs = [sfig for sfig in self.subfigs if sfig.axes != []]
+        self.subfigs = []
         self.lines = []
         self.patches = []
         self.texts = []
