3860a2a5d68238b06a61a9af1d8ad91b1aafbb76
Thomas A Caswell <tcaswell@gmail.com>
2022-08-07 16:55:50 -0400

    ENH: add c++ code + Python API to get char -> font mapping

--- src/ft2font.h
+++ src/ft2font.h
@@ -108,6 +108,7 @@ class FT2Font
     FT_UInt get_char_index(FT_ULong charcode, bool fallback);
     void get_cbox(FT_BBox &bbox);
     PyObject* get_path();
+    bool index_with_glyph(long charcode, int& index) const;
 
     FT_Face const &get_face() const
     {
